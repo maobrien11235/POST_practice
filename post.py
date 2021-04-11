4a0bef2998a30c6a27cc003cad67cc06aad5e384
Matthew O'Brien <maobrien11235@gmail.com>
2021-04-10 23:34:04 -0500

    ENH: fixed post request. Needed json= instead of data=. Submitted successfully and now I wait to hear back.

--- post.py
+++ post.py
@@ -1,2 +1,14 @@
 import requests
+import json
 
+json_file = open("matt_obrien_resume.json")
+write_data = json.load(json_file)
+
+# Ensure JSON loaded
+print(write_data)
+
+end_point = 'https://contact.plaid.com/jobs'
+r = requests.post(url = end_point, json = test)
+
+print(r.status_code)
+print(r.reason)
